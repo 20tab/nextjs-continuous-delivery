89854d442353903c65592849a74cd07975753637
daniele <daniele.pompa@20tab.com>
2021-10-23 19:20:46 +0200

    Remove comments

--- {{cookiecutter.project_dirname}}/terraform/main.tf
+++ {{cookiecutter.project_dirname}}/terraform/main.tf
@@ -95,32 +95,6 @@ resource "kubernetes_deployment" "frontend" {
             name  = "REACT_ENVIRONMENT"
             value = var.environment
           }
-
-          # resources {
-          #   limits = {
-          #     cpu    = "0.5"
-          #     memory = "512Mi"
-          #   }
-          #   requests = {
-          #     cpu    = "250m"
-          #     memory = "50Mi"
-          #   }
-          # }
-
-          # liveness_probe {
-          #   http_get {
-          #     path = "/nginx_status"
-          #     port = 80
-
-          #     http_header {
-          #       name  = "X-Custom-Header"
-          #       value = "Awesome"
-          #     }
-          #   }
-
-          #   initial_delay_seconds = 3
-          #   period_seconds        = 3
-          # }
         }
       }
     }
