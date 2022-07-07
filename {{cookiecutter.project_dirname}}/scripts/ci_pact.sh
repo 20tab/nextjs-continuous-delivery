e287efb7c9caa2f4364d2ce12dda99ea900346c0
daniele20tab <daniele@20tab.com>
2022-07-08 01:38:31 +0200

    Fix pact ci

--- {{cookiecutter.project_dirname}}/scripts/ci_pact.sh
+++ {{cookiecutter.project_dirname}}/scripts/ci_pact.sh
@@ -3,11 +3,14 @@
 if [ "${VAULT_ADDR}" != "" ]; then
   apk update 1> /dev/null
 
-  apk add jq 1> /dev/null
+  apk add curl jq 1> /dev/null
+
+  curl https://releases.hashicorp.com/vault/${VAULT_VERSION:=1.11.0}/vault_${VAULT_VERSION}_linux_386.zip --output vault.zip
+  unzip vault.zip
 
   export VAULT_TOKEN="$(./vault write -field=token auth/gitlab-jwt-${VAULT_PROJECT_PATH}/login role=gitlab-jwt-${VAULT_PROJECT_PATH}-envs-${ENV_SLUG} jwt=${CI_JOB_JWT_V2})"
 
-  pact_secrets=`vault kv get -format='json' -field=data ${VAULT_PROJECT_PATH}/envs/${ENV_SLUG}/pact`
+  pact_secrets=`./vault kv get -format='json' -field=data ${VAULT_PROJECT_PATH}/envs/${ENV_SLUG}/pact`
 
   export PACT_BROKER_BASE_URL=`echo ${pact_secrets} | jq -r .pact_broker_url`
   export PACT_BROKER_PASSWORD=`echo ${pact_secrets} | jq -r .pact_broker_password`
