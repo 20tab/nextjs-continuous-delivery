95652316694c3aa8090105e6f9c999c18ef53255
Matteo Vitali <trottomv@gmail.com>
2024-01-10 12:38:19 +0100

    Configure healthcheck endpoint rewrite path

--- {{cookiecutter.project_dirname}}/pages/api/health.ts
+++ {{cookiecutter.project_dirname}}/pages/api/health.ts
@@ -1,5 +1,5 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next/types'
 
 export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ status: 'ok' })
+  res.status(204).send('')
 }
