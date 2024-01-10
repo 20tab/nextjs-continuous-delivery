5878353a62e6969737c304596ae75e587646a061
Matteo Vitali <trottomv@gmail.com>
2024-01-10 15:22:54 +0100

    Fix import

--- {{cookiecutter.project_dirname}}/pages/api/health.ts
+++ {{cookiecutter.project_dirname}}/pages/api/health.ts
@@ -1,4 +1,4 @@
-import type { NextApiRequest, NextApiResponse } from 'next/types'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default function handler(_req: NextApiRequest, res: NextApiResponse) {
   res.status(204).send('')
