083df45a70459b7a5f8f1423930cea96bcc9a470
Matteo Vitali <trottomv@gmail.com>
2024-01-12 17:16:47 +0100

    TG-964 Expose healthcheck endpoint (#67)
    * Configure healthcheck endpoint rewrite path
    
    * Fix import
    
    * Add healthcheck tests

--- {{cookiecutter.project_dirname}}/pages/api/health.ts
+++ {{cookiecutter.project_dirname}}/pages/api/health.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ status: 'ok' })
+  res.status(204).send('')
 }
